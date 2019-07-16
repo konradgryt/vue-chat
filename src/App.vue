1de8c5ff73a8a8dba9dcaedf5c66ff2e22e74274
konradgryt <konrad.gryt@gmail.com>
2019-07-16 16:18:01 +0200

    Add Login component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -29,3 +25,8 @@
   color: #42b983;
 }
 </style>
+
+<!--    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
